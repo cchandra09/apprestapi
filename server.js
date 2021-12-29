188c2b29f34d1e01c73a3227003489aed0d4e26d
candra-sachio09 <candrasachio@gmail.com>
2021-12-29 14:37:33 +0700

    Create Routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
 const port = 3000;
 app.listen(port, () => {
     console.log('Sercer Started on port')
