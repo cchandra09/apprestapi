45fb9a14ed98eff9426b8d7c108efc7409911950
candra-sachio09 <candrasachio@gmail.com>
2022-01-06 09:23:45 +0700

    Delete Comment Useless

--- server.js
+++ server.js
@@ -8,15 +8,11 @@ const app = express();
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-// app.use(morgan('dev'));
-// app.use(cors());
 
 //panggil routes
 var routes = require('./routes');
 routes(app);
 
-//daftarkan menu routes dari index
-// app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
